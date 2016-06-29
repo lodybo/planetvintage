d5219f7299d575ad2b9b622af5e0381f5cc3e303
Lody Borgers <lody.borgers@capgemini.com>
2016-06-29 10:09:04 +0200

    Replaced page header with automatically centered hero element and improved soem of the theming

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
-$(function () {
-  console.log("ready to go!");
-})
+;(function ($) {
+  // First, vertically center the jumbotron
+  centerJumbotron();
+
+  // Add an eventlistener in case the window height changes
+  $(window).resize(function () {
+    centerJumbotron();
+  });
+  
+
+})(jQuery);
+
+function centerJumbotron() {
+    // Get elements
+    var hero = $(".hero");
+    var jumbotron = $(".jumbotron");
+
+    // First set the height of the hero to that of the image
+    hero.height($(".hero img").innerHeight());
+
+    // Get half point of elements
+    var heroHalfPoint = hero.innerHeight() / 2;
+    var jumbotronHalfPoint = jumbotron.innerHeight() / 2;
+    
+    // Set the top margin of the jumbotron to the half of the hero, and correct it with half of the jumbotron
+    jumbotron.css("margin-top", heroHalfPoint - jumbotronHalfPoint)
+  }
