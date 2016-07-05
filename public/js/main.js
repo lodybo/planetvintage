d3b67a244e52eee34f7ad651a190473018507201
Lody Borgers <lody.borgers@capgemini.com>
2016-07-05 17:17:45 +0200

    Added failsafe for centering of jumbotron, so that it will never hide the main menu

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,7 @@ function centerJumbotron() {
     var heroHalfPoint = hero.innerHeight() / 2;
     var jumbotronHalfPoint = jumbotron.innerHeight() / 2;
     
-    // Set the top margin of the jumbotron to the half of the hero, and correct it with half of the jumbotron
-    jumbotron.css("margin-top", heroHalfPoint - jumbotronHalfPoint)
+    // Set the top margin of the jumbotron to the half of the hero, and correct it with half of the jumbotron, only if that equation is more than 0
+    var newMargin = heroHalfPoint - jumbotronHalfPoint >= 0 ? heroHalfPoint - jumbotronHalfPoint : 0;
+    jumbotron.css("margin-top", newMargin);
   }
