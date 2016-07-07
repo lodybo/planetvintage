4195d9ae9ecc148fa6c925266c1cf0568077f868
Lody Borgers <lody.borgers@capgemini.com>
2016-07-07 17:27:23 +0200

    Removed jumbotron and added PV! logo, which is centered in hero

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,37 @@
 ;(function ($) {
-  // First, vertically center the jumbotron
-  centerJumbotron();
+  // First, vertically center the heroImg
+  centerheroImg();
 
   // Add an eventlistener in case the window height changes
   $(window).resize(function () {
-    centerJumbotron();
+    centerheroImg();
   });
 
   // Also add an event listener when the hero image has loaded, we may need to adjust the height of the hero and such the spacing of the page
-  $(".hero img").load(centerJumbotron);
+  $(".hero img").load(centerheroImg);
   
 
 })(jQuery);
 
-function centerJumbotron() {
+function centerheroImg() {
     // Get elements
     var hero = $(".hero");
-    var jumbotron = $(".jumbotron");
+    var heroImg = $(".hero img");
 
     // First set the height of the hero to that of the image
-    hero.height($(".hero img").innerHeight());
+    hero.height(heroImg.innerHeight());
 
     // Get half point of elements
-    var heroHalfPoint = hero.innerHeight() / 2;
-    var jumbotronHalfPoint = jumbotron.innerHeight() / 2;
+    var heroHeightHalfPoint = hero.innerHeight() / 2;
+    var heroImgHeightHalfPoint = heroImg.innerHeight() / 2;
+
+    var heroWidthHalfPoint = hero.innerWidth() / 2;
+    var heroImgWidthHalfPoint = heroImg.innerWidth() / 2;
     
-    // Set the top margin of the jumbotron to the half of the hero, and correct it with half of the jumbotron, only if that equation is more than 0
-    var newMargin = heroHalfPoint - jumbotronHalfPoint >= 0 ? heroHalfPoint - jumbotronHalfPoint : 0;
-    jumbotron.css("margin-top", newMargin);
+    // Set the top margin of the heroImg to the half of the hero, and correct it with half of the heroImg, only if that equation is more than 0
+    var newTopMargin = heroHeightHalfPoint - heroImgHeightHalfPoint >= 0 ? heroHeightHalfPoint - heroImgHeightHalfPoint : 0;
+    var newLeftMargin = heroWidthHalfPoint - heroImgWidthHalfPoint >= 0 ? heroWidthHalfPoint - heroImgWidthHalfPoint : 0;
+    
+    heroImg.css("margin-top", newTopMargin);
+    heroImg.css("margin-left", newLeftMargin);
   }
